e49f61dc169a6d919c3039b8d77657a3ee53bddd
Caco0 <cacotpds@gmail.com>
2024-06-24 14:09:15 -0300

    Initial commit Environment configuration, implementation and styling of the components: Home, Navbar and Footer

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'blogAtp';
+}
